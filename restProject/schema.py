cc2c4863fc4ba2ce5aed98c5e27dc912e967db54
visor517 <visor517@yandex.ru>
2021-09-27 14:55:12 +0300

    added graphQL DjangoObjectType

--- restProject/schema.py
+++ restProject/schema.py
@@ -1,15 +1,20 @@
-from graphene import Schema, ObjectType, String
+from graphene import Schema, ObjectType, String, List
+from graphene_django import DjangoObjectType
+
+from users.models import User
+
+
+class UserDjangoType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 
 class Query(ObjectType):
-    goodbye = String()
-    hello = String(name=String())
+    all_users = List(UserDjangoType)
 
-    def resolve_goodbye(root, info):
-        return f'goodbye'
-
-    def resolve_hello(root, info, name="Bob"):
-        return f'Hello {name}!'
+    def resolve_all_users(self, info):
+        return User.objects.all()
 
 
 schema = Schema(query=Query)
