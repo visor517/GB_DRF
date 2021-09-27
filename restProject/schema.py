c65408d3340cb3fdf4e823ab7c3612c08b050615
visor517 <visor517@yandex.ru>
2021-09-27 17:11:11 +0300

    added hw10 done

--- restProject/schema.py
+++ restProject/schema.py
@@ -1,6 +1,7 @@
-from graphene import Schema, ObjectType, String, List
+from graphene import Schema, ObjectType, String, List, Int, Mutation, ID, Field
 from graphene_django import DjangoObjectType
 
+from todos.models import Project, ToDo
 from users.models import User
 
 
@@ -10,11 +11,54 @@ class UserDjangoType(DjangoObjectType):
         fields = '__all__'
 
 
+class ProjectDjangoType(DjangoObjectType):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class ToDoDjangoType(DjangoObjectType):
+    class Meta:
+        model = ToDo
+        fields = '__all__'
+
+
 class Query(ObjectType):
     all_users = List(UserDjangoType)
+    all_projects = List(ProjectDjangoType)
+    all_todos = List(ToDoDjangoType)
+    todos_by_project_id = List(ToDoDjangoType, id=Int())
 
     def resolve_all_users(self, info):
         return User.objects.all()
 
+    def resolve_all_projects(self, info):
+        return Project.objects.all()
 
-schema = Schema(query=Query)
+    def resolve_all_todos(self, info):
+        return ToDo.objects.all()
+
+    def resolve_todos_by_project_id(self, info, id):
+        return ToDo.objects.filter(project_id=id)
+
+
+class ToDoMutation(Mutation):
+    class Arguments:
+        pk = ID()
+        description = String(required=True)
+
+    todo = Field(ToDoDjangoType)
+
+    @classmethod
+    def mutate(cls, root, info, pk, description):
+        todo = ToDo.objects.get(pk=pk)
+        todo.description = description
+        todo.save()
+        return ToDoMutation(todo=todo)
+
+
+class Mutation(ObjectType):
+    update_todo_description = ToDoMutation.Field()
+
+
+schema = Schema(query=Query, mutation=Mutation)
